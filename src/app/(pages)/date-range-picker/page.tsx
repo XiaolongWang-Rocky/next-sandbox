1da3fcba2224d2c2d7a6ac20c2f428be5ba52a2c
Rocky Wang <wangrocky89@gmail.com>
2023-11-21 22:04:51 +1100

    charts demo

--- src/app/(pages)/date-range-picker/page.tsx
+++ src/app/(pages)/date-range-picker/page.tsx
@@ -1,25 +1,45 @@
 'use client'
+import { Dialog, DialogContent, DialogActions, Button, Box } from '@mui/material';
 import React, {useState} from 'react'
 import { DateRange } from 'react-date-range'
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 
 export default function Page() {
-    const [dateRange, setDateRange] = useState([
-        {
-          startDate: new Date(),
-          endDate: new Date(),
-          key: 'selection'
-        }
-    ]);
+    const [dateRange, setDateRange] = useState({
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection'
+    })
+    const [open, setOpen] = useState(false)
+    const handleApply = () => {
+      setOpen(false)
+      console.log(dateRange.startDate.toISOString())
+      dateRange.endDate.setHours(23, 59, 59, 999)
+      console.log(dateRange.endDate.toISOString())
+    }
 
-    console.log(dateRange[0]?.startDate?.toISOString())
-    console.log(dateRange[0]?.endDate?.toISOString())
+    // console.log(dateRange[0]?.startDate?.toISOString())
+    // console.log(dateRange[0]?.endDate?.toISOString())
       
-    return <DateRange
-    editableDateInputs={true}
-    onChange={item => setDateRange([item.selection])}
-    moveRangeOnFirstSelection={false}
-    ranges={dateRange}
-    />
+    return <Box>
+      <Dialog
+      open={open}
+      onClose={()=>setOpen(false)}
+      >
+        <DialogContent>
+          <DateRange
+          editableDateInputs={true}
+          onChange={item => setDateRange(item.selection)}
+          moveRangeOnFirstSelection={false}
+          ranges={[dateRange]}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={()=>setOpen(false)}>Cancel</Button>
+          <Button onClick={handleApply}>Apply</Button>
+        </DialogActions>
+      </Dialog>
+      <Button onClick={()=>setOpen(true)}>Open range picker</Button>
+    </Box>
 }
--- src/app/(pages)/react-charts/page.tsx
+++ src/app/(pages)/react-charts/page.tsx
@@ -1,5 +1,66 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'none'
+    },
+    title: {
+      display: false
+    },
+  },
+  scales: {
+    y: {
+        display: false
+    }
+  }
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map((item, index) => Math.floor(Math.random()*10) * index),
+      borderColor: 'white',
+      borderWidth: 6,
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map((item, index) => Math.floor(Math.random()*14) * index),
+      borderColor: 'yellow',
+      borderWidth: 1,
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 export default function Page() {
-    return <p>react charts</p>
+  return <Line options={options} data={data} />;
 }

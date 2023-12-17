336547feff5168e0d884fbafb230d51cc2f4ca46
Rocky Wang <rocky@tipmi.comm>
2023-12-18 08:54:08 +1100

    auto-scroll fixed up

--- src/app/(pages)/auto-scroll/page.tsx
+++ src/app/(pages)/auto-scroll/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import { Box } from '@mui/material'
+import { useState } from 'react'
 
 export default function Page() {
     const dataList = Array.from({length: 10}, (_, i) => i+1)
+    const [category, setCategory] = useState(1)
     const scrollToX = (x: any) => {
-        console.log(x)
-        scroller.scrollTo(x, {
+        setCategory(x)
+        scroller.scrollTo('item,' + x, {
             duration: 500,
             delay: 0,
             smooth: 'easeInOutQuart',
@@ -14,25 +16,44 @@ export default function Page() {
         })
     }
 
-    return <Element id='scroll-container' style={{height: '500px', width: '300px', margin: '30px auto', overflow: 'auto', border: '2px solid gray'}}>
-        {
-            dataList.map((item)=><Element
-            name={'item' + item}
-            key={item}
-            onClick={()=>scrollToX('item' + item)}
+    return <Box sx={{height: '400px', width: '300px', margin: '30px auto', border: '2px solid gray'}}>
+        <Element id='scroll-container' style={{height: '100%', overflow: 'auto'}}>
+            <Box
+            sx={{
+                backgroundColor: 'brown',
+                color: 'white',
+                margin: '5px 0',
+                textAlign: 'center',
+            }}
             >
-                <Box
-                sx={{
-                    backgroundColor: 'brown',
-                    color: 'white',
-                    margin: '5px 0',
-                    textAlign: 'center',
-                    height: '150px'
-                }}
+                item 0
+            </Box>
+            {
+                dataList.map((item)=><Element
+                name={'item,' + item}
+                key={item}
+                onClick={()=>scrollToX(item)}
                 >
-                    item {item}
-                </Box>
-            </Element>)
-        }
-    </Element>
+                    <Box
+                    sx={{
+                        backgroundColor: 'brown',
+                        color: 'white',
+                        margin: '5px 0',
+                        textAlign: 'center',
+                    }}
+                    >
+                        item {item}
+                        {item === category && <Content />}
+                    </Box>
+                </Element>)
+            }
+        </Element>
+    </Box>
+
+}
+
+function Content() {
+    return <Box sx={{height: '300px', width: '100%'}}>
+        Content
+    </Box>
 }

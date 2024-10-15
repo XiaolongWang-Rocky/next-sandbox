0a6b3445f4a1a173b3321ef33fb41a33f686d22a
Rocky Wang <wangrocky89@gmail.com>
2024-10-15 23:20:08 +1100

    bi-direct infinite scroll

--- src/app/(pages)/duo-direction-infinite-scroll/page.tsx
+++ src/app/(pages)/duo-direction-infinite-scroll/page.tsx
@@ -1,21 +1,18 @@
 'use client'
 import { Box, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import useInfiniteScroll, { ScrollDirection } from 'react-easy-infinite-scroll-hook'
+import { useEffect, useRef, useState } from "react";
 
 export default function Page() {
     const [dataList, setDataList] = useState([100])
     const [loadingPrev, setLoadingPrev] = useState(false)
     const [loadingNext, setLoadingNext] = useState(false)
     const [initialized, setInitialized] = useState(false)
-    // const [hasMore, setHasMore] = useState({
-    //     up: true,
-    //     down: true
-    // })
+    const [isInitScrolled, setIsInitScrolled] = useState(false)
+    const wrapperRef = useRef(null)
+    const selectedRef = useRef(null)
 
     const loadMore = async (direction: string) => {
         if (!initialized) return
-        console.log(direction)
         if (direction === 'down') {
             console.log('loadingNext: ', loadingNext)
             if (!loadingNext) {
@@ -45,44 +42,65 @@ export default function Page() {
         }
     }
 
-    // useInfiniteScroll<HTMLDivElement>({
-    //     next: loadMore,
-    //     rowCount: dataList.length,
-    //     hasMore,
-    //     windowScroll: true,
-    //     scrollThreshold: '5px'
-    // });
-
     useEffect(() => {
         setTimeout(() => {
             setDataList([95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105])
             setInitialized(true)
-        }, 1500)
+        }, 2000)
     }, [])
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollTop = window.scrollY || document.documentElement.scrollTop;
-            const scrollBottom = window.innerHeight + scrollTop;
+    const handleScroll = () => {
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
+        const scrollBottom = window.innerHeight + scrollTop;
 
-            // 顶部加载更多
-            if (scrollTop === 0) {
-                loadMore('up');
-            }
-
-            // 底部加载更多
-            if (scrollBottom >= document.documentElement.scrollHeight) {
-                loadMore('down');
-            }
+        // 顶部加载更多
+        if (scrollTop === 0) {
+            loadMore('up');
         }
 
+        // 底部加载更多
+        if (scrollBottom >= document.documentElement.scrollHeight) {
+            loadMore('down');
+        }
+    }
+
+    useEffect(() => {
         if (initialized) {
             window.addEventListener('scroll', handleScroll)
+            if(!isInitScrolled) {
+                selectedRef.current.scrollIntoView({behavior: 'instant'})
+                setIsInitScrolled(true)
+            }
         }
-        return window.removeEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [initialized, dataList, isInitScrolled]) //'dataList' must be added as the dependency, otherwise its value won't change in the listener's callback function
+
+    useEffect(() => {
+        console.log(wrapperRef.current.scrollHeight)
     }, [dataList])
 
     return <Box
+        sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '100%'
+        }}
+    >
+        <Typography
+        sx={{
+            width: '300px',
+            height: '50px',
+            background: 'lightgray',
+            position: 'sticky',
+            top: 0,
+            zIndex: 2
+        }}
+        >Header</Typography>
+        <Box
+        ref={wrapperRef}
         sx={{
             display: 'flex',
             flexDirection: 'column',
@@ -90,19 +108,23 @@ export default function Page() {
             alignItems: 'center',
             width: '100%'
         }}
-    >
+        >
         {
             initialized && dataList.map(item => <Box
+                data-item={item}
                 key={item}
                 sx={{
                     width: '300px',
                     height: `${(item % 3 + 1) * 100}px`,
                     backgroundColor: '#a2fdff',
-                    borderRadius: '6px'
+                    borderRadius: '6px',
+                    scrollMarginTop: '50px'
                 }}
+                ref={item === 98 ? selectedRef : null}
             >
                 <Typography sx={{ fontWeight: 600, textAlign: 'center', pt: '20px', fontSize: '20px' }}>{item}</Typography>
             </Box>)
         }
+        </Box>
     </Box>
 }
